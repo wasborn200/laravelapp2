4e6e6743fe17e7cb13094f7abb5e004e7df78636
wasborn200 <archibald.ff1@gmail.com>
2021-07-09 07:36:36 +0900

    crud implemented

--- resources/views/person/index.blade.php
+++ resources/views/person/index.blade.php
@@ -1,20 +1,18 @@
 @extends('layouts.helloapp')
 
-@section('title', 'Index')
+@section('title', 'Person.index')
 
 @section('menubar')
     @parent
-    インデックス
+    インデックスページ
 @endsection
 
 @section('content')
     <table>
-        <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
+        <tr><th>Data</th></tr>
         @foreach ($items as $item)
             <tr>
-                <td>{{$item->name}}</td>
-                <td>{{$item->mail}}</td>
-                <td>{{$item->age}}</td>
+                <td>{{$item->getData()}}</td>
             </tr>
         @endforeach
     </table>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloController;
+use App\Http\Controllers\PersonController;
 use App\Http\Middleware\HelloMiddleware;
 
 
@@ -10,5 +11,29 @@ Route::get('/', function () {
 });
 
 Route::get('hello', [HelloController::class, 'index']);
-
 Route::post('hello', [HelloController::class, 'post']);
+
+Route::get('hello/add', [HelloController::class, 'add']);
+Route::post('hello/add', [HelloController::class, 'create']);
+
+Route::get('hello/edit', [HelloController::class, 'edit']);
+Route::post('hello/edit', [HelloController::class, 'update']);
+
+Route::get('hello/del', [HelloController::class, 'del']);
+Route::post('hello/del', [HelloController::class, 'remove']);
+
+Route::get('hello/show', [HelloController::class, 'show']);
+
+Route::get('person', [PersonController::class, 'index']);
+
+Route::get('person/find', [PersonController::class, 'find']);
+Route::post('person/find', [PersonController::class, 'search']);
+
+Route::get('person/add', [PersonController::class, 'add']);
+Route::post('person/add', [PersonController::class, 'create']);
+
+Route::get('person/edit', [PersonController::class, 'edit']);
+Route::post('person/edit', [PersonController::class, 'update']);
+
+Route::get('person/del', [PersonController::class, 'delete']);
+Route::post('person/del', [PersonController::class, 'remove']);
